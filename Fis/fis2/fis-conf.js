642f8021630124aa3b4cd4498e5b3b2edafbeac9
SidKwok <oceankwok@hotmail.com>
2016-06-18 21:35:13 +0800

    ud fis3

---
+++ Fis/fis2/fis-conf.js
@@ -0,0 +1,4 @@
+/**
+* @file fis2的配置文件
+* @author sid
+*/
--- Fis/fis3/fis-conf.js
+++ Fis/fis3/fis-conf.js
@@ -3,6 +3,26 @@
 * @author sid
 */
 
+// 模块化选项
+
+// commonjs
+// define()
+// require('')
+// require.async('')
+// require.async([])
+fis.hook('commonjs' {
+    // 配置项
+});
+
+// AMD
+// define()
+// require([]);
+// require('');
+// npm install fis3-hook-amd
+fis.hook('amd', {
+  // 配置项
+});
+
 // 匹配widget下所有子目录了下的js文件
 fis.match('/widget/**/*.js', {
     release: '/static/$0' //匹配文件的输出路径，$0的意思是上面那整一串的路径
@@ -44,38 +64,66 @@ fis.match('/widget/**', {
     useSameNameRequire: true
 });
 
+// 雪碧图
+// 这个是默认开启的
+fis.match('*.css', {
+    useSprite: true
+});
+
 // 文件是否使用编译缓存
 // 当设置使用编译缓存，每个文件的编译结果均会在磁盘中保存以供下次编译使用。设置为 false 后，则该文件每次均会被编译。
 fis.match('**.html', {
-      useCache: false
+    useCache: false
 });
 
 // 对文件进行压缩处理
 fis.match('*.js', {
-  optimizer: fis.plugin('uglify-js')
+    optimizer: fis.plugin('uglify-js')
 });
 
 fis.match('*.css', {
-  optimizer: fis.plugin('clean-css')
+    optimizer: fis.plugin('clean-css')
 });
 
 fis.match('*.png', {
-  optimizer: fis.plugin('png-compressor')
+    optimizer: fis.plugin('png-compressor')
 });
 
 // 对文件进行打包处理
 fis.match('::package', {
-  packager: fis.plugin('some-pack-plugin'), // 挂载一个打包插件
-  spriter: fis.plugin('csssprites') // FIS2 默认启用 csssprites
+    packager: fis.plugin('some-pack-plugin'), // 挂载一个打包插件
+    spriter: fis.plugin('csssprites') // FIS2 默认启用 csssprites
 });
 
 fis.match('/widget/*.js', {
-  packTo: '/static/widget_aio.js' // 匹配文件打包到此文件中
+    packTo: '/static/widget_aio.js' // 匹配文件打包到此文件中
 });
 
 fis.match(['/widget/a/*.js', '/widget/b/*.js'], { // 用数组控制打包的东西
-  packTo: '/static/widget_aio.js'
+    packTo: '/static/widget_aio.js'
 });
+/*也可以酱紫*/
+fis.media('prod')
+    // 启用打包插件，必须匹配 ::package
+    .match('::package', {
+        packager: fis.plugin('map'),
+        spriter: fis.plugin('csssprites', {
+            layout: 'matrix',
+            margin: '15'
+        })
+    })
+    .match('*.js', {
+        packTo: '/static/all_others.js'
+    })
+    .match('*.css', {
+        packTo: '/staitc/all_others.css'
+    })
+    .match('/widget/**/*.js', {
+        packTo: '/static/all_comp.js'
+    })
+    .match('/widget/**/*.css', {
+        packTo: '/static/all_comp.css'
+    });
 
 
 // 设置项目发布方式
@@ -85,3 +133,23 @@ fis.match('**', { // 所有文件
           to: '/home/work/www' // 将部署到服务器的这个目录下
       })
   });
+
+// 使用前端模版
+fis.match('**.tmpl', {
+     parser: fis.plugin('utc'), // invoke `fis-parser-utc`
+     isJsLike: true
+  });
+
+// 不release某些资源
+fis.match('*.inline.css', {
+  // 设置 release 为 FALSE，不再产出此文件
+  release: false
+});
+
+// ignore一些不希望被构建的资源
+fis.set('project.ignore', [
+  'output/**',
+  'node_modules/**',
+  '.git/**',
+  '.svn/**'
+]);
